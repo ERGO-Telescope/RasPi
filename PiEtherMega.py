c1f845e1869fee6435799ca1b42926938df0dae4
Daniel Huertas <huertasdanny@gmail.com>
2013-06-13 15:06:35 -0400

    Python codes

---
+++ PiEtherMega.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+#coding=utf-8
+import serial
+import binascii
+import PiEtherMegaShield as shield
+import numpy as np
+
+
+
+#declare variables
+numEvent=0
+
+##necessary switch function
+class switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        """Return the match method once, then stop"""
+        yield self.match
+        raise StopIteration
+    
+    def match(self, *args):
+        """Indicate whether or not to enter a case suite"""
+        if self.fall or not args:
+            return True
+        elif self.value in args: # changed for v1.5, see below
+            self.fall = True
+            return True
+        else:
+            return False
+## Main loop function
+
+def loopFunc():
+    NextState=1
+    while True:
+        State=NextState
+        for case in switch(State):
+            if case(1):
+                print ("going to Shieldinit")
+                shield.ShieldInit()
+                NextState=2
+                break
+            if case(2):
+                if shield.CollectPosition():
+                    shield.SendMsg(setTIM2_On)
+                    NextState=3
+                break
+            if case(3):
+                if shield.EventFound():
+                    numEvent+=1
+                    NextState=4
+                break
+            #case 4-6 crypto stuff
+
+loopFunc()
